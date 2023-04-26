efffa0e7e4d5ec4f845678db2795ad7b9735f534
Mathis Zerari <matiszerari@icloud.com>
2023-04-26 13:28:03 +0200

    searchbar done

--- client/src/components/searchbar/SearchBar.vue
+++ client/src/components/searchbar/SearchBar.vue
@@ -1,32 +1,34 @@
 <template>
   <html>
     <head>
-    <link href="/dist/output.css" rel="stylesheet">
-  </head>
-    <body>
-      <div>
-        <h1>FakeTube</h1>
-        <form class="bg-dark shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div class="div-input w-2/3 mx-auto relative">
-            <input
-            class="w-full py-2 pl-10 pr-3 leading-5 rounded-md border border-gray-300 bg-white text-gray-500 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
-            type="text"
-            placeholder="Search"
-            />
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <svg class="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+      <link href="/dist/output.css" rel="stylesheet">
+    </head>
+    <body class="bg-gray-900">
+      <div class="container mx-0 px-0">
+        <h1 class="text-red-500 text-2xl font-bold mt-5">FakeTube</h1>
+        <div class="w-2/3 mx-auto relative">
+
+          <input v-model="msg" @change="logMsg" class="w-full py-2 pl-10 pr-3 leading-5 border border-gray-400 bg-gray-800 text-white rounded-l-full rounded-r-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent" type="text" placeholder="Search" />
+          <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none text-gray-400"></div>
+          <div class="loop absolute inset-y-0 right-5  flex items-center pointer-events-none">
+            <div class=" h-full w-16 rounded-r-full rounded-l-none border border-gray-400 bg-gray-700 ml-2">
+              <svg class="w-5 h-5 mt-2 ml-4" viewBox="0 0 20 20" fill="none" stroke="white">
                 <path
-                  fill-rule="evenodd"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
                   d="M13.447 12.764l4.83 4.828a1 1 0 01-1.415 1.415l-4.828-4.83a7 7 0 111.413-1.413zM7 12a5 5 0 100-10 5 5 0 000 10z"
-                  clip-rule="evenodd"
                 />
               </svg>
             </div>
           </div>
-        </form>
-
-        <div v-if="msg.length > 0"  class="div-parent">
-          <div class='div-red'></div>
+          <div class="ml-auto h-full"></div>
+        </div>
+        
+        <div v-if="msg.length > 0" class="flex justify-center items-center">
+          <div class="bg-blue rounded-b-lg w-2/3 h-64 flex justify-center items-center">
+            <div class="w-32 h-32 rounded-full bg-red-500"></div>
+          </div>
         </div>
       </div>
     </body>
@@ -34,53 +36,36 @@
 </template>
 
 <script setup>
-
-import { ref } from "vue";
-// eslint-disable-next-line no-unused-vars
+  import { ref } from 'vue';
 const msg = ref("")
-</script>
 
+function logMsg() {
+    console.log(msg.value);
+  }
+
+  // eslint-disable-next-line vue/no-export-in-script-setup
+  // export default {
+  //   setup() {
+  //     const msg = ref('');
+
+  //     function logMsg() {
+  //       console.log(msg.value);
+  //     }
+
+  //     return { msg, logMsg };
+  //   }
+  // }
+
+</script>
 <style scoped>
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
 
-
-body{
-  margin-top: 10rem;
-  background: #0F0F0F; 
+.loop {
+  margin-right: -1.295rem;
 }
-h1 {
-color: red;
-font-size: 1.5rem;
-position: absolute;
-}
-.div-input{
-text-align: center;
-}
-input {
-width: 20rem;
-/*
-background: #121212;
-text-align: center;
-width: 20rem;
-margin-top: 1.35rem;
-height: 1rem;
-color: white;
- */
-}
-.div-red {
-border-radius: 0 0 15px 15px;
-background:#171717; 
-margin-top: 0.1rem;
-width: 20.5rem;
-height:15rem;
-display:flex;
-justify-content:center;
-}
-.div-parent {
-display: flex;
-justify-content: center;
-align-items: center;
+svg {
+  z-index: 9999;
 }
 </style>
