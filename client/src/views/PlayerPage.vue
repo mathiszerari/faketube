df78609acb56e772133165cdfb20b0cdb27c8430
Kisat0 <ghanembenyoucef@gmail.com>
2023-04-26 14:23:15 +0200

    Update player page style, and add height and width 100% in the body

--- client/src/views/PlayerPage.vue
+++ client/src/views/PlayerPage.vue
@@ -1,105 +1,219 @@
 <template>
-    <div class="video-player">
-      <div class="video-container">
-        <video ref="video" class="video-player-video" :src="videoUrl" @play="handlePlay" @pause="handlePause"></video>
-      </div>
-      <div class="video-player__controls">
-        <button class="video-player__play-pause" @click="handlePlayPauseClick">{{ isPlaying ? 'Pause' : 'Play' }}</button>
-        <div class="video-player__progress-bar" @click="handleProgressBarClick">
-          <div class="video-player__progress-bar__fill" :style="{ width: progressBarWidth }"></div>
+    <div class="player-page-content">
+        <div class="video-player-container">
+            <div class="video-player">
+                <div class="video-container">
+                    <video ref="video" class="video-player-video" :src="videoUrl" @play="handlePlay"
+                        @pause="handlePause"></video>
+                </div>
+                <div class="video-player__controls">
+                    <button class="video-player__play-pause" @click="handlePlayPauseClick">{{ isPlaying ? 'Pause' : 'Play'
+                    }}</button>
+                    <div class="video-player__progress-bar" @click="handleProgressBarClick">
+                       <progress :max="100" :value="progressBarWidth"></progress>
+                    </div>
+                    <span class="video-player__time">{{ currentTime }} / {{ duration }}</span>
+                    <button class="video-player__full-screen" @click="fullScreenVideo">Full Screen</button>
+                </div>
+            </div>
+            <h1>OMG !! YOU ARE NOT GONNA BELIEVE THIS ! (I DELETE THIS VIDEO IN 24H)</h1>
+            <div class="video-player-bottom-controls">
+                <div class="video-player-channel-content">
+                    <div class="video-player-channel-infos">
+                        <img class="video-player-channel-img" :src="channelImg" />
+                        <div class="video-player-channel">
+                                <span class="video-player-channel-name">DarkSasuke</span>
+                                <span class="video-player-channel-subscribers">113K</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bottom-controls-buttons">
+                    <button>Like</button>
+                    <button>Dislike</button>
+                    <button>Share</button>
+                </div>
+            </div>
+
         </div>
-        <span class="video-player__time">{{ currentTime }} / {{ duration }}</span>
-      </div>
+        <div class="video-list">
+            <div>
+                <p>VIDEOOOOO</p>
+            </div>
+        </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     data() {
-      return {
-        isPlaying: false,
-        currentTime: '00:00',
-        duration: '00:00',
-        progressBarWidth: 100,
-        videoUrl: '/src/assets/plage.mp4' 
-      };
+        return {
+            isPlaying: false,
+            currentTime: '00:00',
+            duration: '00:00',
+            progressBarWidth: 0,
+            videoUrl: '/src/assets/plage.mp4',
+            channelImg: '/src/assets/channelimg.png'
+        };
     },
     methods: {
-      handlePlay() {
-        this.isPlaying = true;
-      },
-      handlePause() {
-        this.isPlaying = false;
-      },
-      handlePlayPauseClick() {
-        if (this.isPlaying) {
-          this.$refs.video.pause();
-        } else {
-          this.$refs.video.play();
+        handlePlay() {
+            this.isPlaying = true;
+        },
+        handlePause() {
+            this.isPlaying = false;
+        },
+        handlePlayPauseClick() {
+            if (this.isPlaying) {
+                this.$refs.video.pause();
+            } else {
+                this.$refs.video.play();
+            }
+        },
+        handleProgressBarClick(event) {
+            const progressBar = event.currentTarget;
+            const progressBarWidth = progressBar.offsetWidth;
+            const clickX = event.offsetX;
+            const percentage = clickX / progressBarWidth;
+            const currentTime = percentage * this.$refs.video.duration;
+            this.$refs.video.currentTime = currentTime;
+        },
+        updateTime() {
+            const video = this.$refs.video;
+            const currentTime = Math.floor(video.currentTime);
+            const minutes = Math.floor(currentTime / 60);
+            const seconds = currentTime - minutes * 60;
+            const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+            this.currentTime = formattedTime;
+
+            const duration = Math.floor(video.duration);
+            const durationMinutes = Math.floor(duration / 60);
+            const durationSeconds = duration - durationMinutes * 60;
+            const formattedDuration = `${durationMinutes.toString().padStart(2, '0')}:${durationSeconds.toString().padStart(2, '0')}`;
+            this.duration = formattedDuration;
+
+            const percentage = Math.floor((currentTime / duration) * 100);
+            this.progressBarWidth = `${percentage}`;
+        },
+        fullScreenVideo() {
+            const video = this.$refs.video;
+            if (video.requestFullscreen) {
+                video.requestFullscreen();
+            } else if (video.webkitRequestFullscreen) { /* Safari */
+                video.webkitRequestFullscreen();
+            } else if (video.msRequestFullscreen) { /* IE11 */
+                video.msRequestFullscreen();
+            }
         }
-      },
-      handleProgressBarClick(event) {
-        const progressBar = event.currentTarget;
-        const progressBarWidth = progressBar.offsetWidth;
-        const clickX = event.offsetX;
-        const percentage = clickX / progressBarWidth;
-        const currentTime = percentage * this.$refs.video.duration;
-        this.$refs.video.currentTime = currentTime;
-      },
-      updateTime() {
-        const video = this.$refs.video;
-        const currentTime = Math.floor(video.currentTime);
-        const minutes = Math.floor(currentTime / 60);
-        const seconds = currentTime - minutes * 60;
-        const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-        this.currentTime = formattedTime;
-  
-        const duration = Math.floor(video.duration);
-        const durationMinutes = Math.floor(duration / 60);
-        const durationSeconds = duration - durationMinutes * 60;
-        const formattedDuration = `${durationMinutes.toString().padStart(2, '0')}:${durationSeconds.toString().padStart(2, '0')}`;
-        this.duration = formattedDuration;
-  
-        const percentage = (currentTime / duration) * 100;
-        this.progressBarWidth = `${percentage}%`;
-      }
     },
     mounted() {
-      this.$refs.video.addEventListener('timeupdate', this.updateTime);
+        this.$refs.video.addEventListener('timeupdate', this.updateTime);
     },
     beforeUnmount() {
-      this.$refs.video.removeEventListener('timeupdate', this.updateTime);
+        this.$refs.video.removeEventListener('timeupdate', this.updateTime);
     }
-  };
-  </script>
+};
+</script>
   
-  <style>
-  .video-player {
+<style scoped>
+.player-page-content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 10vh;
+    flex-wrap: nowrap;
+}
+
+.video-player-container {
+    width: 70%;
+    height: 100%;
+}
+
+.video-player {
     position: relative;
     width: 100%;
-    height: 0;
-    padding-bottom: 56.25%; /* 16:9 aspect ratio */
-  }
-  
-  .video-container {
+    height: 550px;
+}
+
+.video-container {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-  }
-  
-  .video-player-video {
+}
+
+.video-player-video {
     width: 100%;
     height: 100%;
     object-fit: cover;
-  }
-  
-  .video-player__controls {
+}
+
+.video-player__controls {
     position: absolute;
     bottom: 0;
     left: 0;
     width: 100%;
-   
-  }  
-  </style>
+    display: flex;
+    flex-direction: row;
+    margin-left: 5px;
+}
+
+.video-player__time {
+    margin-left: 10px;
+}
+
+.video-player-bottom-controls {
+    display: flex;
+    flex-direction: row;
+    width:100%;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.video-player-channel-img {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.video-player-channel{
+    display:flex;
+    flex-direction: column;
+    margin-left:5px;
+}
+
+.video-player-channel-infos{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+}
+
+.video-list{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+
+.video-player-channel-content{
+    margin-right: auto;
+}
+
+h1{
+    font-size: 20px;
+    font-weight: bold;
+    color:white;
+    margin:10px 0px;
+}
+
+.bottom-controls-buttons button{
+margin:0px 10px;
+}
+</style>
