44a969284531f565f78f066b8cf8addfaa675ace
Val <pro.mvaldo@gmail.com>
2023-04-26 17:46:44 +0200

    feat : add style and responsive to videos display

--- client/src/components/Timeline/TheAdvancedTimeline.vue
+++ client/src/components/Timeline/TheAdvancedTimeline.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {reactive, onMounted, ref} from "vue"
 import { useFetch } from '@vueuse/core'
+import VideoCard from "./VideoCard.vue";
 
 const { isFetching, error, data:products } = useFetch('https://fakestoreapi.com/products') // TO DO Need to change URL with the true URL
 
@@ -8,17 +8,25 @@ const { isFetching, error, data:products } = useFetch('https://fakestoreapi.com/
 </script>
 
 <template>
-    <div v-if="!isFetching">
-        <div v-for="(product) in JSON.parse(products)" :key="product.id">
-            {{product.id}}{{ product.title }}
+    <div class="bg-zinc-800 pt-10">
+        <h1 class="text-white font-bold text-2xl pb-5">Recommendation</h1>
+        <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5" v-if="!isFetching">
+            <div v-for="(product) in JSON.parse(products)" :key="product.id">
+                <router-link to="/">
+                    <video-card :video="product"></video-card>
+                </router-link>
+    
+            </div>
         </div>
-    </div>
-    <div v-else>
-        <div>
-        <!-- TO DO add code link to Loading Videos -->
-            Loading...
+        <div v-else>
+            <div>
+            <!-- TO DO add code link to Loading Videos -->
+                Loading...
+            </div>
         </div>
+        
     </div>
+   
     
 </template>
 
--- client/src/components/Timeline/VideoCard.vue
+++ client/src/components/Timeline/VideoCard.vue
@@ -9,16 +9,26 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
-        <img class="video__thumbnail" :src="video.image" :alt="video.title">
-        <div>
-            <div class="publisher__avatar">
+    <div class="aspect-video text-white">
+        <img class="w-max aspect-video object-cover rounded-2xl" 
+        :src="video.image" 
+        :alt="video.title">
+        <div class="flex flex-row gap-4 mt-4">
+            <div class="publisher__avatar bg-zinc-700 rounded-full aspect-square">
             </div>
-            <div class="video__info">
-                <span>{{ video.title }}</span>
-                <span>{{ video.publisher }}</span>
-                <span>{{ video.views }}de vues - {{ video.created_at }}</span>
+            <div class="video__info flex flex-col">
+                <span class="font-bold">{{ video.title }}</span>
+                <span class="text-zinc-500">{{ video.category }}</span>
+                <span class="text-zinc-500">{{ video.rating.count }} de vues - {{ video.price }}</span>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+
+.publisher__avatar{
+    width: 50px;
+    height: 50px;
+}
+</style>
