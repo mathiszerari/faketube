f7b53a1a8fac7cdff1900836f859a67af255d309
Val <pro.mvaldo@gmail.com>
2023-04-26 14:25:32 +0200

    create video card component structure

---
+++ client/src/components/Timeline/VideoCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const props = defineProps({
+    video: {
+        type: Object,
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <div>
+        <img class="video__thumbnail" :src="video.image" :alt="video.title">
+        <div>
+            <div class="publisher__avatar">
+            </div>
+            <div class="video__info">
+                <span>{{ video.title }}</span>
+                <span>{{ video.publisher }}</span>
+                <span>{{ video.views }}de vues - {{ video.created_at }}</span>
+            </div>
+        </div>
+    </div>
+</template>
