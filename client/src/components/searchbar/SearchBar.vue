7a1e007ab18a8d3b1754afe08ebb1c74726dde22
Mathis Zerari <matiszerari@icloud.com>
2023-04-26 14:38:46 +0200

    search and suggest but not in fonction of letters

--- client/src/components/searchbar/SearchBar.vue
+++ client/src/components/searchbar/SearchBar.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/no-parsing-error -->
 <template>
   <html>
     <head>
@@ -7,7 +8,7 @@
       <div class="container mx-0 px-0">
         <h1 class="text-red-500 text-2xl font-bold mt-5">FakeTube</h1>
         <div class="w-2/3 mx-auto relative">
-          <input v-model="msg" @change="logMsg" class="w-full py-2 pl-10 pr-3 leading-5 border border-gray-400 bg-gray-800 text-white rounded-l-full rounded-r-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent" type="text" placeholder="Search" />
+          <input v-model="msg" @keyup="logMsg" class="w-full py-2 pl-10 pr-3 leading-5 border border-gray-400 bg-gray-800 text-white rounded-l-full rounded-r-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent" type="text" placeholder="Search" />
           <div class="loop absolute inset-y-0 right-5  flex items-center pointer-events-none">
             <div class="h-full w-16 rounded-r-full border border-gray-400 bg-gray-700 ml-2">
               <svg class="w-5 h-5 mt-2 ml-4" viewBox="0 0 20 20" fill="none" stroke="white">
@@ -25,8 +26,8 @@
         <div v-if="msg.length > 0" class="flex justify-center items-center">
           <div class="bg-blue w-2/3 mx-auto relative flex justify-center items-center">
             <div class="w-full rounded-xl mt-1 boder h-64 bg-gray-700">
-              <a href="{{ content.link }}" v-for="content in contents" :key="content.text" class="left-1 mt-25 w-64">
-                <svg class="loop-sugg w-4 mt-2 ml-4" viewBox="0 0 20 20" fill="none" stroke="white">
+              <a href="{{ content.link }}" v-for="content in contents.slice(0, 4)" :key="content.text" class="block left-1  py-4">
+                <svg class="w-5 mt-2 ml-4 px-0 ml-0 mr-0" viewBox="0 0 20 20" fill="none" stroke="white">
                   <path
                     stroke-width="2"
                     stroke-linecap="round"
@@ -34,7 +35,7 @@
                     d="M13.447 12.764l4.83 4.828a1 1 0 01-1.415 1.415l-4.828-4.83a7 7 0 111.413-1.413zM7 12a5 5 0 100-10 5 5 0 000 10z"
                   />
                 </svg>
-                <span class="mx-5">{{ content.text }}</span><br>
+                <span class="mx-5 px-3">{{ content.text }}</span><br>
               </a>
             </div>
           </div>
@@ -55,22 +56,20 @@
     { text: 'iPhone 12', link: 'https://www.youtube.com/results?search_query=iphone+12' },
     { text: 'iPhone 11', link: 'https://www.youtube.com/results?search_query=iphone+11' },
     { text: 'iPhone X', link: 'https://www.youtube.com/results?search_query=iphone+X' },
-  ])
+  ]);
 
-  // eslint-disable-next-line vue/no-export-in-script-setup
-  // export default {
-  //   setup() {
-  //     const msg = ref('');
-
-  //     function logMsg() {
-  //       console.log(msg.value);
-  //     }
-
-  //     return { msg, logMsg };
-  //   }
-  // }
-
+  const logMsg = () => {
+    console.log(msg.value);
+  };
 </script>
+
+<script>
+  export default {
+    name: 'MyComponent',
+    // autres options du composant
+  };
+</script>
+
 <style scoped>
 @tailwind base;
 @tailwind components;
