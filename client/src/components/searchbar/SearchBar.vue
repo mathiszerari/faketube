489e778d2f101810a02bc79f78af465117559d66
Mathis Zerari <matiszerari@icloud.com>
2023-04-26 18:58:58 +0200

    cross working

--- client/src/components/searchbar/SearchBar.vue
+++ client/src/components/searchbar/SearchBar.vue
@@ -1,54 +1,51 @@
 <!-- eslint-disable vue/no-parsing-error -->
 <template>
-  <html>
-    <head>
-      <link href="/dist/output.css" rel="stylesheet">
-    </head>
-    <body class="bg-gray-900">
-      <div class="container mx-0 px-0">
-        <h1 class="text-red-500 text-2xl font-bold mt-5">FakeTube</h1>
-        <div class="w-2/3 mx-auto relative">
-          <input v-model="msg" @keyup="logMsg" class="w-full py-2 pl-10 pr-3 leading-5 border border-gray-400 bg-gray-800 text-white rounded-l-full rounded-r-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent" type="text" placeholder="Search" />
-          <div class="loop absolute inset-y-0 right-5  flex items-center pointer-events-none">
-            <div class="h-full w-16 rounded-r-full border border-gray-400 bg-gray-700 ml-2">
-              <svg class="w-5 h-5 mt-2 ml-4" viewBox="0 0 20 20" fill="none" stroke="white">
-                <path
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.447 12.764l4.83 4.828a1 1 0 01-1.415 1.415l-4.828-4.83a7 7 0 111.413-1.413zM7 12a5 5 0 100-10 5 5 0 000 10z"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-        
-        <div v-if="msg.length > 0" class="flex justify-center items-center">
-          <div class="bg-blue w-2/3 mx-auto relative flex justify-center items-center">
-            <div class="w-full rounded-xl mt-1 boder h-64 bg-gray-700">
-              <a href="{{ content.link }}" v-for="content in contents.slice(0, 4)" :key="content.text" class="block left-1  py-4">
-                <svg class="w-5 mt-2 ml-4 px-0 ml-0 mr-0" viewBox="0 0 20 20" fill="none" stroke="white">
-                  <path
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M13.447 12.764l4.83 4.828a1 1 0 01-1.415 1.415l-4.828-4.83a7 7 0 111.413-1.413zM7 12a5 5 0 100-10 5 5 0 000 10z"
-                  />
-                </svg>
-                <span class="mx-5 px-3">{{ content.text }}</span><br>
-              </a>
-            </div>
-          </div>
+  <div class="container mx-0 px-0 bg-gray-900">
+    <h1 class="text-red-500 text-2xl font-bold mt-5">FakeTube</h1>
+    <div class="w-2/3 mx-auto relative">
+      <input v-model="msg" @keyup="logMsg" class="w-full py-2 pl-10 pr-3 leading-5 border border-gray-400 bg-gray-800 text-white rounded-l-full rounded-r-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent" type="text" placeholder="Search" />
+      <div class="loop absolute inset-y-0 right-5  flex items-center">
+        <button v-if="msg.length > 0" @click="clearmsg" class="p-4" id="cross-clear">
+          <span class="absolute top-1/2 w-5 h-0.5 bg-gray-100 transform -translate-x-1/2 -translate-y-1/2 rotate-45"></span>
+          <span class="absolute top-1/2 w-0.5 h-5 bg-gray-100 transform -translate-x-1/2 -translate-y-1/2 rotate-45"></span>
+        </button>
+        <div class="h-full w-16 rounded-r-full border border-gray-400 bg-gray-700 ml-2">
+          <svg class="w-5 h-5 mt-2 m-4" viewBox="0 0 20 20" fill="none" stroke="white">
+            <path
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.447 12.764l4.83 4.828a1 1 0 01-1.415 1.415l-4.828-4.83a7 7 0 111.413-1.413zM7 12a5 5 0 100-10 5 5 0 000 10z"
+            />
+          </svg>
         </div>
       </div>
-    </body>
-  </html>
+    </div>
+    
+    <div v-if="msg.length > 0" class="flex justify-center items-center">
+      <div class="bg-blue w-2/3 mx-auto relative flex justify-center items-center">
+        <div class="w-full rounded-xl mt-1 boder h-64 bg-gray-700">
+          <a href="{{ content.link }}" v-for="content in contents.slice(0, 4)" :key="content.text" class="block left-1  py-4">
+            <svg class="w-5 mt-2 ml-4 px-0 mx-5 ml-0 ml-5" viewBox="0 0 20 20" fill="none" stroke="white">
+              <path
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.447 12.764l4.83 4.828a1 1 0 01-1.415 1.415l-4.828-4.83a7 7 0 111.413-1.413zM7 12a5 5 0 100-10 5 5 0 000 10z"
+              />
+            </svg>
+            <span class="mx-5">{{ content.text }}</span><br>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
 
-  const msg = ref('');
+const msg = ref('');
   const contents = ref([
     { text: 'iPhone 15', link: 'https://www.youtube.com/results?search_query=iphone+15' },
     { text: 'iPhone 14', link: 'https://www.youtube.com/results?search_query=iphone+14' },
@@ -61,20 +58,13 @@
   const logMsg = () => {
     console.log(msg.value);
   };
-</script>
-
-<script>
-  export default {
-    name: 'MyComponent',
-    // autres options du composant
+  const clearmsg = () => {
+    msg.value = '';
   };
+
 </script>
 
 <style scoped>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
 .loop {
   margin-right: -1.295rem;
 }
@@ -84,5 +74,12 @@ svg {
 }
 a {
   color: white;
+}
+#cross-clear {
+  cursor: pointer;
+}
+.container {
+  padding: 1rem 0rem 50rem 0rem;
+  max-width: 100%;
 }
 </style>
