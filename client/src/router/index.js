189147fd542f10f96107f057e3e69ae9857bf3ae
Smachy <pro.diegocdossantos@gmail.com>
2023-04-27 15:51:03 +0200

    first part conditions score

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TestView from "@/views/TestView.vue";
-import SearchVideo from "@/views/SearchVideo.vue"
+import SearchVideo from "@/views/SearchVideoView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- client/src/views/SearchVideoView.vue
+++ client/src/views/SearchVideoView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import {reactive, ref, onMounted, computed} from 'vue';
 import {useFetch} from "@vueuse/core";
+import * as dayjs from 'dayjs';
+
 
 // Requête sur le serveur
 const {isFetching, error, data:video} = useFetch('http://localhost:8080/searchVideo')
@@ -9,18 +11,74 @@ const formattedVideo = computed(()=>{
     return JSON.parse(video.value)
 })
 
+function checkChannel(channel, word){
+    if(channel==word){
+        return 500;
+    }
+    return 0;
+}
+
+function checkTitle(title, word){
+    if(title.includes(word)){
+        return 100;
+    }
+    return 0;
+}
+
+function checkTag(tag, word) {
+    if (tag.includes(word)) {
+        return 75;
+    }
+    return 0;
+}
+
+function checkDescription(description, word) {
+    if (description.includes(word)) {
+        return 25;
+    }
+    return 0;
+}
+
+function checkDate(uploadDate) {
+    let dateDiff = dayjs().diff(dayjs(uploadDate), "day")
+    if(dateDiff<=1){
+        return 100;
+    }
+    if(dateDiff<=7){
+        return 30;
+    }
+    return 0;   
+}
+function checkViews(viewsNumber){
+    if(viewsNumber){
+
+    }
+}
+
+function checkSubscribers(number){
+
+}
+
+function getScore(data, userInput){
+    let score = 0
+    const userWords = userInput.split("+")
+    userWords.forEach(word => {
+        // score+=checkChannel()
+    });
+}
+
+
 </script>
 
 
 <template>
     <main>
         <div>
+            {{ formattedVideo }}
             <span v-for="(video,index) in formattedVideo.message" :key="index">
-                {{ video.tags }}
+
+                {{  }}
             </span>
         </div>
-        <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
     </main>
   </template>
