2bec8c10181d599b414745930ac9236190425b00
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-04-27 21:41:11 +0200

    route video and upload

--- server/src/routes/videoRoute.js
+++ server/src/routes/videoRoute.js
@@ -1,30 +1,25 @@
-const fileUpload = require('express-fileupload');
 const fs = require("fs");
-
-const db = require('../utils/database')
-const conn = require('./conn')
-
 const path = require("path")
 
+const db = require('../utils/database')
 const app = require('../utils/app')
-/*
-app.use(express.static('videos')); //to access the files in public folder
-app.use(cors()); // it enables all cors requests
-app.use(fileUpload());
-*/
+
+const removeLastDirPart = dirname => path.parse(dirname).dir
+var newpath = removeLastDirPart(__dirname)
+newpath = removeLastDirPart(newpath)
 
 app.app.get("/videos", (req, res) => {
     db.db.query("SELECT * FROM videos",
     function(err, videos) {
-    if (err) {
-        return res.status(500).json({ message: "Failed to retrieve videos" });
-    } else {
-        return res.status(200).json({ videos });
-    }
-})
+        if (err) {
+            return res.status(500).json({ message: "Failed to retrieve videos" });
+        } else {
+            return res.status(200).json({ videos });
+        }
+    })
 });
 
-app.app.get("/video/:id", async (req, res) => {
+app.app.get("/video/:id", (req, res) => {
     db.db.query(`SELECT * FROM videos WHERE id=${req.params.id}`,
     function(err, video) {
         if (err) {
@@ -32,10 +27,7 @@ app.app.get("/video/:id", async (req, res) => {
         } else if (video.length < 1) {
             return res.status(200).json({ message: "This video isn't available anymore" });
         } else {
-            const removeLastDirPart = dirname => path.parse(dirname).dir
-            var newpath = removeLastDirPart(__dirname)
-            newpath = removeLastDirPart(newpath)
-            return res.sendFile(newpath+"/"+video[0].video_path), (error) => {
+            return res.sendFile(newpath + video[0].video_path), (error) => {
                 if (error) {
                     res.status(200).json({ message: "This miniature does not exist" });
                 }
@@ -44,7 +36,7 @@ app.app.get("/video/:id", async (req, res) => {
     })
 });
 
-app.app.get("/miniature/:id", async (req, res) => {
+app.app.get("/miniature/:id", (req, res) => {
 	db.db.query(`SELECT * FROM videos WHERE id=${req.params.id}`,
     function(err, miniature) {
         if (err) {
@@ -52,10 +44,7 @@ app.app.get("/miniature/:id", async (req, res) => {
         } else if (miniature.length < 1) {
             return res.status(200).json({ message: "This miniature isn't available anymore" });
         } else {
-            const removeLastDirPart = dirname => path.parse(dirname).dir
-            var newpath = removeLastDirPart(__dirname)
-            newpath = removeLastDirPart(newpath)
-            return res.sendFile(newpath+"/"+miniature[0].miniature_path), (error) => {
+            return res.sendFile(newpath + miniature[0].miniature_path), (error) => {
                 if (error) {
                     res.status(200).json({ message: "This miniature does not exist" });
                 }
