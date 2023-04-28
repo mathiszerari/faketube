3e3c21d9d78af1a2adae6a9a92dc9170acab7aeb
mathis <mathis.renaudin95@gmail.com>
2023-04-28 15:55:42 +0200

    v1

--- client/src/components/profile.vue
+++ client/src/components/profile.vue
@@ -1,135 +1,213 @@
-template>
-      <v-container align="center" justify="center" fill-height>
-        <v-row align="center" justify="center">
-          <div class="text-center">
-            <h1 class="mb-2">Profile Card</h1>
+<template>
+  <div class="profile dark-theme">
+    <div class="profile-header">
+      <img :src="profileImage" class="profile-image" alt="Profile Image" />
+      <div class="profile-titles">
+        <h1 class="profile-title">{{ profileUsername }}</h1>
+        <p class="profile-description">Membre depuis le {{ memberSince }}</p>
+      </div>
+      <button class="profile-logout-btn" @click="logout">↪ Déconnexion</button>
+    </div>
+    <div class="profile-content">
+      <div class="profile-section">
+        <h2 class="profile-section-title">Informations de compte</h2>
+        <div class="profile-info">
+          <div class="profile-info-label">Pseudo :</div>
+          <div class="profile-info-value">
+            {{ profileUsername }} <button class="profile-edit-btn" @click="editUsername">Modifier</button>
           </div>
-        </v-row>
-        <v-row class="bg-img" justify="space-around">
-          <v-col cols="12" class="mt-8">
-            <v-card width="400">
-              <v-img
-                  height="200px"
-                  src="https://cdn.pixabay.com/photo/2021/07/09/06/52/lavender-6398415_960_720.jpg"
-              >
-                <v-app-bar
-                    class="mt-8"
-                    flat
-                    color="rgba(0, 0, 0, 0)"
-                >
-                  <v-avatar size="100">
-                    <img
-                        alt="user"
-                        src=""
-                    >
-                  </v-avatar>
-
-                  <v-spacer></v-spacer>
-
-                  <v-menu offset-y left>
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn
-                          v-bind="attrs"
-                          v-on="on"
-                          color="white"
-                          icon
-                      >
-                        <v-icon>mdi-dots-vertical</v-icon>
-                      </v-btn>
-                    </template>
-                    <v-list>
-                      <v-list-item href="https://codingfactory.fr/">
-                        <v-list-item-title>Edit</v-list-item-title>
-                      </v-list-item>
-                    </v-list>
-                  </v-menu>
-
-                </v-app-bar>
-
-                <v-card-title class="white--text mt-8">
-                  <p class="ml-3">
-                    Hamza Ben Alouane
-                  </p>
-                </v-card-title>
-              </v-img>
-
-              <v-card-text>
-
-                <div class="font-weight-bold ml-8 mb-2">
-                  Details
-                </div>
-
-                <v-list two-line>
-                  <v-list-item href="https://codingfactory.fr/">
-                    <v-list-item-icon>
-                      <v-icon color="indigo">
-                        mdi-phone
-                      </v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                      <v-list-item-title>07 67 13 21 70</v-list-item-title>
-                      <v-list-item-subtitle>Mobile</v-list-item-subtitle>
-                    </v-list-item-content>
-
-                    <v-list-item-icon>
-                      <v-icon>mdi-message-text</v-icon>
-                    </v-list-item-icon>
-                  </v-list-item>
-
-                  <v-divider inset></v-divider>
-
-                  <v-list-item href="https://codingfactory.fr/">
-                    <v-list-item-icon>
-                      <v-icon color="indigo">
-                        mdi-email
-                      </v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                      <v-list-item-title>hbenalouane19@gmail.com</v-list-item-title>
-                      <v-list-item-subtitle>Email</v-list-item-subtitle>
-                    </v-list-item-content>
-
-                    <v-list-item-icon>
-                      <v-icon>mdi-message-text</v-icon>
-                    </v-list-item-icon>
-                  </v-list-item>
-
-                  <v-divider inset></v-divider>
-
-                  <v-list-item href="https://codingfactory.fr/">
-                    <v-list-item-icon>
-                      <v-icon color="indigo">
-                        mdi-map-marker
-                      </v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                      <v-list-item-title>Coding Factory</v-list-item-title>
-                      <v-list-item-subtitle>Online</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+        </div>
+        <div class="profile-info">
+          <div class="profile-info-label">Email :</div>
+          <div class="profile-info-value">
+            {{ profileEmail }} <button class="profile-edit-btn" @click="editEmail">Modifier</button>
+          </div>
+        </div>
+        <div class="profile-info">
+          <div class="profile-info-label">Mot de passe :</div>
+          <div class="profile-info-value">
+            ******** <button class="profile-edit-btn" @click="editPassword">Modifier</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <p>© 2023 Faketube</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "app-profile-contact-card"
+  name: 'Profile',
+  data() {
+    return {
+      profileImage: 'https://education.l214.com/wp-content/uploads/2020/09/15-animaux-moches-04.jpg',
+      profileUsername: 'Mathis95c',
+      profileEmail: 'mathis95c@gmail.com',
+      memberSince: '28 avril 2023'
+    }
+  },
+  methods: {
+    editUsername() {
+      const newUsername = prompt('Nouveau pseudo :', this.profileUsername)
+      if (newUsername !== null) {
+        this.profileUsername = newUsername
+      }
+    },
+    editEmail() {
+      let newEmail = prompt('Nouvel email :', this.profileEmail)
+      while (newEmail !== null && !isValidEmail(newEmail)) {
+        alert('Adresse e-mail invalide.')
+        newEmail = prompt('Nouvel email :')
+      }
+      if (newEmail !== null) {
+        this.profileEmail = newEmail
+      }
+    },
+    editPassword() {
+      const newPassword = prompt('Nouveau mot de passe :')
+      if (newPassword !== null) {
+        // En réalité, il faudrait appeler une API pour modifier le mot de passe enregistré côté serveur.
+        console.log('Le mot de passe a été modifié avec succès.')
+      }
+    },
+    logout() {
+      window.location.href = 'index.html'
+    }
+  }
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailRegex.test(email)
 }
 </script>
 
 <style>
-.bg-img {
-  background-image: url("https://wallpapercave.com/wp/wp2725720.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center top;
-  border-radius: 3px;
+
+
+body {
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  margin: 0;
+  padding: 0;
+  background-color: #1a1a1a;
+  color: #fff;
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
 }
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 40px;
+
+}
+
+.profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.profile-header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 40px;
+  width: 100%;
+}
+
+.profile-image {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 20px;
+}
+
+.profile-title {
+  font-size: 36px;
+  display : flex;
+  justify-content : center;
+  align-items : center;
+}
+
+.profile-description {
+  font-size: 24px;
+  margin: 0;
+  color: #bbb;
+}
+
+.profile-content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.profile-section {
+  margin-bottom: 20px;
+}
+
+.profile-section-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.profile-info {
+  display: flex;
+  margin-bottom: 10px;
+  align-items: center;
+}
+
+.profile-info-label {
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.profile-info-value {
+  color: #bbb;
+}
+
+.profile-info-value button {
+  color: black;
+}
+
+.btn {
+  display: inline-block;
+  background-color: #2196f3;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  font-size: 16px;
+  cursor: pointer;
+  margin-left: 10px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.btn:hover {
+  background-color: #0c7cd5;
+}
+
+.btn:active {
+  background-color: #0c7cd5;
+  transform: translateY(1px);
+}
+
+.footer {
+  width: 100%;
+  padding: 10px 20px;
+  background-color: #0d0d0d;
+  text-align: center;
+  font-size: 14px;
+  color: #ccc;
+  margin-top: auto;
+}
+
 </style>
