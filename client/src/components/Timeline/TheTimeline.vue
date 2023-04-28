0237c8b0e8d1e47f7c110543dfe2ee4179bec55a
Coralie Pereira <coralie-p@outlook.fr>
2023-04-28 15:48:20 +0200

    feat:link vue timeline with node route

--- client/src/components/Timeline/TheTimeline.vue
+++ client/src/components/Timeline/TheTimeline.vue
@@ -1,42 +1,16 @@
-<!-- <template>
-     <div class="bg-zinc-800 pt-10">
-        <h2 class="text-white font-bold text-2xl pb-5" >Video</h2>
-        <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5" v-if="!isFetching"> 
-            <div  v-for="(product) in JSON.parse(products)" :key="product.id">
-                <router-link to="/video">
-                    <video-card :video="product"></video-card>
-                </router-link>
-            </div>
-        </div>
-    </div>
-</template>
-
-
-<script>
-
-
-</script>
-
-<style>
-
-</style> -->
-
-
-
 <template>
     <div class="bg-zinc-800 pt-10">
         <h2 class="text-white font-bold text-2xl pb-5">Video</h2>
         <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5" v-if="!isFetching">
-            <div v-for="(product) in JSON.parse(products)" :key="product.id">
+            <div v-for="(video) in formattedVideo.message" :key="video.id">
                 <router-link to="/video">
-                    <video-card :video="product"></video-card>
+                    <video-card :video="video"></video-card>
                 </router-link>
 
             </div>
         </div>
         <div v-else>
             <div>
-            <!-- TO DO add code link to Loading Videos -->
                 Loading...
             </div>
         </div>
@@ -49,7 +23,11 @@
 <script setup>
 import { useFetch } from '@vueuse/core'
 import VideoCard from "./VideoCard.vue";
+ 
+const { isFetching, error, data:videos } = useFetch('http://localhost:8080/getTimelineVideos%27')
 
-const { isFetching, error, data:products } = useFetch('https://fakestoreapi.com/products') 
+const formattedVideo = computed(()=>{
+    return formattedVideo.message
+})
 
 </script>
