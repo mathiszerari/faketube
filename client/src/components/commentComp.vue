82c412660119411088dd1bcb7a614b4ee564468e
Kisat0 <ghanembenyoucef@gmail.com>
2023-05-01 17:56:39 +0200

    Convert into components

---
+++ client/src/components/commentComp.vue
@@ -0,0 +1,30 @@
+<template>
+          <div class="comments-list-item">
+            <img alt="channel img" :src="channelImg" />
+            <div class="comment-content">
+              <div class="comment-infos">
+                <span>Channel name</span>
+                <span> 1 hour ago</span>
+                <!-- Add edited or not here ? -->
+              </div>
+              <p class="comment-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda veritatis aliquam
+                aspernatur dolorum deserunt praesentium quaerat, sit minima autem, omnis iure. Animi
+                perferendis, eveniet repudiandae modi laboriosam dicta. Laudantium, id!
+              </p>
+              <div class="bottom-comment">
+                <button>Like</button>
+                <button>Dislike</button>
+                <span>Answer</span>
+              </div>
+            </div>
+          </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
---
+++ client/src/components/videoItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="video-list-item">
+        <img alt="thumbnail" :src="thumbnailVideo" class="video-list-thumbnail" />
+        <div class="video-list-item-infos">
+          <span class="video-list-item-title">Title video</span>
+          <span class="video-list-item-channel-name">VlogUS</span>
+          <div>
+            <span class="video-list-item-views">2.1m</span>
+            <span class="video-list-item-date">2 days ago</span>
+          </div>
+        </div>
+      </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
