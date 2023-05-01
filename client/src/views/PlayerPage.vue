8f5bd983a99afa2f5e434dae28e459da648a814e
Kisat0 <ghanembenyoucef@gmail.com>
2023-05-01 17:57:12 +0200

    Change router path with only watch page, and add sound feature

---
+++ client/src/views/PlayerPage.vue
@@ -0,0 +1,458 @@
+<template>
+  <div class="player-page-content">
+    <div class="video-player-container">
+      <div class="video-player">
+        <div class="video-container">
+          <video
+            ref="video"
+            class="video-player-video"
+            :src="videoUrl"
+            @play="handlePlay"
+            @pause="handlePause"
+          ></video>
+        </div>
+        <div class="video-player__controls">
+          <button class="video-player__play-pause" @click="handlePlayPauseClick">
+            {{ isPlaying ? 'Pause' : 'Play' }}
+          </button>
+          <div class="video-player__progress-bar" @click="handleProgressBarClick">
+            <progress :max="100" :value="progressBarWidth"></progress>
+          </div>
+          <span class="video-player__time">{{ currentTime }} / {{ duration }}</span>
+          <button class="video-player__full-screen" @click="fullScreenVideo">Full Screen</button>
+          <div class="video-player__volume">
+            <button class="video-player__volume-btn" @click="toggleMute">
+              {{ isMuted ? 'Unmute' : 'Mute' }}
+            </button>
+            <input
+              class="video-player__volume-slider"
+              type="range"
+              min="0"
+              max="1"
+              step="0.1"
+              :value="volume"
+              @input="setVolumeWithProgressBar"
+            />
+          </div>
+        </div>
+      </div>
+      <h1>OMG !! YOU ARE NOT GONNA BELIEVE THIS ! (I DELETE THIS VIDEO IN 24H)</h1>
+      <div class="video-player-bottom-controls">
+        <div class="video-player-channel-content">
+          <div class="video-player-channel-infos">
+            <img class="video-player-channel-img" :src="channelImg" />
+            <div class="video-player-channel">
+              <span class="video-player-channel-name">DarkSasuke</span>
+              <span class="video-player-channel-subscribers">113K</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-controls-buttons">
+          <button>Like</button>
+          <button>Dislike</button>
+          <button>Share</button>
+        </div>
+      </div>
+
+      <div class="description-case">
+        <div class="video-informations">
+          <span class="video-views">9,3k views</span>
+          <span class="video-date">2 hours ago</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero adipisci, quo soluta
+          sapiente debitis consectetur, corrupti voluptatum hic nobis, aspernatur itaque aperiam
+          sunt dolor amet fuga quibusdam esse reprehenderit possimus.
+        </p>
+      </div>
+
+      <section class="comments-section">
+        <div class="comments-infos">
+          <span>27 Comments</span>
+          <select>
+            <option value="">Sort by</option>
+            <option value="Top">Top Comments</option>
+            <option value="Recent">Recent Comments</option>
+          </select>
+        </div>
+        <div class="add-comment-row">
+          <img :src="channelImg" alt="channel img" />
+          <input type="text" placeholder="Add a comment..." />
+        </div>
+
+        <div class="comments-list">
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
+      </section>
+    </div>
+    <div class="video-list">
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+    </div>
+  </div>
+</template>
+  
+<script>
+import commentCompVue from '../components/commentComp.vue'
+import videoItemVue from '../components/videoItem.vue'
+
+export default {
+  data() {
+    return {
+      volume: 0.5,
+      isMuted: false,
+      isPlaying: false,
+      currentTime: '00:00',
+      duration: '00:00',
+      progressBarWidth: 0,
+      videoUrl: '/src/assets/episode.mp4',
+      channelImg: '/src/assets/channelimg.png',
+      thumbnailVideo: '/src/assets/thumbnail.jpg'
+    }
+  },
+  methods: {
+    handlePlay() {
+      this.isPlaying = true
+    },
+    handlePause() {
+      this.isPlaying = false
+    },
+    handlePlayPauseClick() {
+      if (this.isPlaying) {
+        this.$refs.video.pause()
+      } else {
+        this.$refs.video.play()
+      }
+    },
+    handleProgressBarClick(event) {
+      const progressBar = event.currentTarget
+      const progressBarWidth = progressBar.offsetWidth
+      const clickX = event.offsetX
+      const percentage = clickX / progressBarWidth
+      const currentTime = percentage * this.$refs.video.duration
+      this.$refs.video.currentTime = currentTime
+    },
+    updateTime() {
+      const video = this.$refs.video
+      const currentTime = Math.floor(video.currentTime)
+      const minutes = Math.floor(currentTime / 60)
+      const seconds = currentTime - minutes * 60
+      const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds
+        .toString()
+        .padStart(2, '0')}`
+      this.currentTime = formattedTime
+
+      const duration = Math.floor(video.duration)
+      const durationMinutes = Math.floor(duration / 60)
+      const durationSeconds = duration - durationMinutes * 60
+      const formattedDuration = `${durationMinutes.toString().padStart(2, '0')}:${durationSeconds
+        .toString()
+        .padStart(2, '0')}`
+      this.duration = formattedDuration
+
+      const percentage = Math.floor((currentTime / duration) * 100)
+      this.progressBarWidth = `${percentage}`
+    },
+    fullScreenVideo() {
+      const video = this.$refs.video
+      if (video.requestFullscreen) {
+        video.requestFullscreen()
+      } else if (video.webkitRequestFullscreen) {
+        /* Safari */
+        video.webkitRequestFullscreen()
+      } else if (video.msRequestFullscreen) {
+        /* IE11 */
+        video.msRequestFullscreen()
+      }
+    },
+    toggleMute() {
+      this.isMuted = !this.isMuted
+      this.$refs.video.muted = this.isMuted
+    },
+    setVolume(event) {
+      this.volume = event.target.value
+      this.$refs.video.volume = this.volume
+    },
+
+    setVolumeWithProgressBar(event) {
+      const progressBarVolume = parseFloat(event.target.value)
+      const volume = isFinite(progressBarVolume) ? progressBarVolume : 1
+      this.volume = volume
+      this.$refs.video.volume = volume
+    }
+  },
+  components: {
+    Comment: commentCompVue,
+    VideoItem: videoItemVue
+  },
+  mounted() {
+    this.$refs.video.addEventListener('timeupdate', this.updateTime)
+  },
+  beforeUnmount() {
+    this.$refs.video.removeEventListener('timeupdate', this.updateTime)
+  }
+}
+</script>
+  
+<style scoped>
+.player-page-content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 10vh;
+  flex-wrap: nowrap;
+}
+
+.video-player-container {
+  width: 65%;
+  height: 100%;
+}
+
+.video-player {
+  position: relative;
+  width: 100%;
+  height: 550px;
+}
+
+.video-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  overflow: hidden;
+  border: 2px solid transparent;
+  border-style: none;
+  box-shadow: 0px 0px 5px 1px rgb(117, 117, 117);
+}
+
+.video-player-video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.video-player__controls {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding-left: 5px;
+  background-color: rgba(0, 0, 0, 0.563);
+}
+
+.video-player__time {
+  margin-left: 10px;
+  white-space: nowrap;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.video-player-bottom-controls {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.video-player-channel-img {
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.video-player-channel {
+  display: flex;
+  flex-direction: column;
+  margin-left: 5px;
+}
+
+.video-player-channel-infos {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+
+.video-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.video-player-channel-content {
+  margin-right: auto;
+}
+
+h1 {
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+  margin: 10px 0px;
+}
+
+.bottom-controls-buttons button {
+  margin: 0px 10px;
+}
+
+.video-player__progress-bar {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.video-player__progress-bar progress {
+  width: 100%;
+  margin: 0 10px;
+  height: 5px;
+}
+
+progress[value]::-webkit-progress-value {
+  background-image: -webkit-linear-gradient(
+      -45deg,
+      transparent 33%,
+      rgba(0, 0, 0, 0.1) 33%,
+      rgba(0, 0, 0, 0.1) 66%,
+      transparent 66%
+    ),
+    -webkit-linear-gradient(top, rgba(255, 255, 255, 0.25), rgba(0, 0, 0, 0.25)),
+    -webkit-linear-gradient(left, rgb(0, 156, 204), rgb(20, 94, 223));
+
+  border-radius: 2px;
+}
+
+.video-list-thumbnail {
+  width: 200px;
+  object-fit: cover;
+  height: 120px;
+}
+
+.video-list-item {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+}
+
+.video-list-item-infos {
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+.video-list-item-infos div {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.video-list-item-title {
+  font-size: 15px;
+  font-weight: bold;
+  color: white;
+}
+
+.description-case {
+  margin-top: 10px;
+}
+
+.video-date {
+  margin-left: 5px;
+}
+
+.comments-section {
+  margin-top: 30px;
+}
+
+.comments-infos span {
+  font-size: 20px;
+  color: white;
+}
+.comments-infos select {
+  margin-left: 20px;
+  background-color: rgb(34, 34, 34);
+  text-align: center;
+  padding: 5px;
+}
+
+.add-comment-row {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 20px;
+}
+.add-comment-row img {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+  background-color: white; /* for transparent png */
+}
+.add-comment-row input {
+  background-color: transparent;
+  border-bottom: 1px solid rgb(138, 138, 138);
+  transition: all 500ms;
+  margin-left: 15px;
+  width: 100%;
+}
+.add-comment-row input:active,
+.add-comment-row input:focus,
+.add-comment-row input:hover {
+  border-bottom: 1px solid rgb(255, 255, 255);
+  outline: 0;
+}
+.comments-list {
+  margin-top: 50px;
+}
+.comments-list-item {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+}
+.comments-list-item img {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+  background-color: white; /* for transparent png */
+}
+.comment-content {
+  margin-left: 15px;
+}
+
+.comment-infos span:first-child {
+  color: white;
+  font-weight: bold;
+}
+
+.comment-text {
+  color: white;
+}
+
+.bottom-comment > * {
+  margin-right: 10px;
+}
+
+.bottom-comment span {
+  color: rgb(0, 157, 255);
+  font-weight: bold;
+}
+</style>
