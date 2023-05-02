ec4beb38e20c7f136485dec20a8ceaeb4652f3e1
NoéHLM <noeheylemadec@gmail.com>
2023-05-02 16:57:24 +0200

    final mardi

--- client/src/views/SearchPage.vue
+++ client/src/views/SearchPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {reactive, ref, onMounted, computed} from 'vue';
 import {useFetch} from "@vueuse/core";
+import * as dayjs from 'dayjs';
 const {isFetching, error, data:video} = useFetch('http://localhost:8080/searchPage')
 
 const formattedVideo = computed(()=>{
@@ -9,15 +10,19 @@ const formattedVideo = computed(()=>{
 </script>
 
 <template>
-    <div v-for="(video,index) in formattedVideo.message" :key="index" class="bg-white shadow overflow-hidden sm:rounded-lg flex">
+    <div v-for="(video,index) in formattedVideo" :key="index" class="bg-white shadow overflow-hidden sm:rounded-lg flex">
         <div class="aspect-w-16 aspect-h-9 flex-shrink-0">
-            <img class="object-cover w-full h-full" :src="video.miniature_path" alt="Miniature de la vidéo">
+          <img class="object-cover w-full h-full" :src="video.miniature_path" alt="Miniature de la vidéo">
         </div>
         <div class="p-4 flex-grow">
-          <h3 class="text-black font-bold mb-2">{{ video.title }}</h3>
-          <p class="text-gray-500 text-sm">{{ video.description }}</p>
-        </div>
-    </div>
+            <h3 class="text-black font-bold mb-2">{{ video.title }}</h3>
+            <p class="text-gray-500 text-sm">{{ video.description }}</p>
+            <p class="text-gray-500 text-sm">{{ video.like_number }} poces blo</p>
+            <p class="text-gray-500 text-sm">{{ video.views }} vues</p>
+            <p class="text-gray-500 text-sm">{{ video.pseudo }} vues</p>
+          </div>
+      </div>
+      
       
       
       
--- server/src/routes/searchPage.js
+++ server/src/routes/searchPage.js
@@ -2,9 +2,12 @@ const app = require('../utils/app')
 const db = require('../utils/database')
 
 var result;
-db.db.query(
-    'SELECT * FROM `videos`',
 
+db.db.query(
+    
+    'SELECT videos.*, users.pseudo AS user_pseudo ' +
+    'FROM videos ' +
+    'INNER JOIN users ON videos.user_id = users.id',
     function(err, results, fields) {
         console.log(results); // results contains rows returned by server
 
