c4c9b6999fda0fee1e761df17b3fbc22a6a6118f
celianloisel <celianloisel@gmail.com>
2023-05-02 08:53:44 +0200

    feature-add-route-with-var-path

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const app = require('./utils/app')
 
 const uploadVideo = require('./routes/uploadVideo')
+const register = require('./routes/profil/register')
 
 const PORT = process.env.PORT || 8080;
 app.app.listen(PORT, () => {
