688d1f6afa2708d98ed54a5a599efd444e4f05f3
Val <pro.mvaldo@gmail.com>
2023-05-03 20:27:53 +0200

    feat: display advanced only when user is conn

--- client/src/components/Timeline/TheAdvancedTimeline.vue
+++ client/src/components/Timeline/TheAdvancedTimeline.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useFetch } from '@vueuse/core'
 import VideoCard from "./VideoCard.vue";
-import { computed } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 import VideoCardSkeleton from './VideoCardSkeleton.vue';
 
 const { isFetching, error, data:videos } = useFetch('http://localhost:8080/getAdvancedTimelineVideos?userId=1')
@@ -9,10 +9,19 @@ const formattedVideo = computed(()=>{
     return JSON.parse(videos.value)
 })
 
+const userId = ref("");
+
+onMounted(()=>{
+    if (localStorage.getItem('id')) {
+        userId.value = localStorage.getItem('id')
+    }
+})
+
+
 </script>
 
 <template>
-    <div>
+    <div v-if="userId !== ''">
         <h2 class="text-white font-bold text-2xl pb-5">Recommendation</h2>
         <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5" v-if="!isFetching">
             <div v-for="(videoData) in formattedVideo?.message" :key="videoData.id">
