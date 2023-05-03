8f16f6778e3d53de20117700f6a43411d5793ec3
marko95stojanovic <stojanovic.marko2004@gmail.com>
2023-05-03 11:34:17 +0200

    features-add-login

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+
+
+<script setup>
+import TheLogin from '../components/TheLogin.vue';
+</script>
+
+<template>
+    <main>
+        <div class="flex justify-center items-center h-screen w-screen">
+        <TheLogin>
+ 
+        </TheLogin>
+    </div>
+    </main>
+</template>
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const app = require('./utils/app')
 
 const uploadVideo = require('./routes/uploadVideo')
+const addNewUser = require('./routes/profil/login')
 
 const PORT = process.env.PORT || 8080;
 app.app.listen(PORT, () => {
