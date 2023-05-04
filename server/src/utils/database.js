d529641e40e30fa90b1d53a2180f0dcc159d27d0
Kisat0 <ghanembenyoucef@gmail.com>
2023-05-04 15:37:36 +0200

    fix/fix pull request

--- server/src/utils/database.js
+++ server/src/utils/database.js
@@ -1,4 +1,4 @@
-/* require('dotenv').config();
+require('dotenv').config();
 var mysql = require('mysql2');
 
 module.exports = {
@@ -7,18 +7,4 @@ module.exports = {
         user: process.env.USER,
         database: process.env.DATABASE
     })
-} */
-
-
-require('dotenv').config();
-var mysql = require('mysql2');
-
-module.exports = {
-    db: mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'faketube',
-        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-    })
-};
+} 
